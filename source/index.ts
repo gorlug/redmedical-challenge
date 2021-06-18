c8017f5ea93fd99ff917dea108021825b743cfe9
Achim Rohn <achim@rohn.eu>
2021-06-18 10:46:30 +0200

    Clean up console logs

--- source/index.ts
+++ source/index.ts
@@ -9,24 +9,17 @@ function readFile(name: string) {
 function unzip() {
     let key = readFile('secret.key') + ''
     key = key.substring(0, key.length - 1)
-    console.log('key', key + '324')
     const iv = readFile('iv.txt')
-    console.log('iv length', iv.length)
     const secret = readFile('secret.enc')
-    console.log('key length', key.length)
 
     const cipherKey = key.substring(0, 32)
-    console.log('cipherKey', cipherKey.length)
 
     const auth = readFile('auth.txt')
 
-
     const decipher = crypto.createDecipheriv('aes-256-gcm', cipherKey, iv)
     const update = decipher.update(secret)
-    console.log('update', typeof update)
     decipher.setAuthTag(auth)
     const final = decipher.final()
-    console.log('final', typeof final)
     const buffer = Buffer.concat([update, final])
     const unzipped = zlib.unzipSync(buffer)
     fs.writeFileSync(__dirname + '/../unzipped.txt', unzipped)
@@ -37,7 +30,6 @@ if (argv.length === 2) {
     console.log('specify sentence and word index, example: npm run start 5 2')
     process.exit(1)
 }
-console.log('args', argv)
 unzip()
 
 const sentenceIndex = Number.parseInt(argv[2])
@@ -45,7 +37,6 @@ const wordIndex = Number.parseInt(argv[3])
 
 const text = fs.readFileSync(__dirname + '/../unzipped.txt', 'utf-8')
 const sentences = text.split('.')
-console.log('sentences', sentences.length)
 if (sentenceIndex >= sentences.length) {
     console.log('sentence index not available')
     process.exit(1)
@@ -56,6 +47,5 @@ if (wordIndex >= words.length) {
     console.log('word index not available')
     process.exit(1)
 }
-console.log('words', words.length)
 const word = words[wordIndex]
-console.log('word', word)
+console.log('word:', word)
